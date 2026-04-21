b5d81c7dfeb1ccccbea336dfcc61665600012056
JustRafau <r@rafau.pl>
2026-04-21 02:24:31 +0200

    Working GPS with a demo in MapScreen.kt

--- app/src/main/java/com/ggs/parkuzpp/location/UserTriggeredGPSService.kt
+++ app/src/main/java/com/ggs/parkuzpp/location/UserTriggeredGPSService.kt
@@ -11,13 +11,14 @@ import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.math.RoundingMode
 
 class UserTriggeredGPSService(private val context: Context) {
 
     private val fusedLocationClient by lazy {
         try {
             LocationServices.getFusedLocationProviderClient(context)
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             null
         }
     }
@@ -26,65 +27,86 @@ class UserTriggeredGPSService(private val context: Context) {
         context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
 
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private suspend fun getLocationViaAndroid(): Location? {
+        return suspendCancellableCoroutine { continuation ->
+            val provider = when {
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) -> {
+                    Log.e("LocationService", "Using GPS provider")
+                    LocationManager.GPS_PROVIDER
+                }
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) -> {
+                    Log.e("LocationService", "Using Network provider")
+                    LocationManager.NETWORK_PROVIDER
+                }
+                else -> {
+                    Log.e("LocationService", "No provider available")
+                    null
+                }
+            }
+
+            if (provider == null) {
+                continuation.resume(null) { cause, _, _ -> (::onCancellation)(cause) }
+                return@suspendCancellableCoroutine
+            }
+
+            Log.e("LocationService", "Requesting location from $provider")
+            locationManager.getCurrentLocation(
+                provider,
+                null,
+                context.mainExecutor
+            ) { location ->
+                if (location != null) {
+                    Log.e("LocationService", "Got fresh location: $location")
+                    continuation.resume(location) { cause, _, _ -> (::onCancellation)(cause) }
+                } else {
+                    // cold fix, try last known
+                    Log.e("LocationService", "Fresh location null, trying last known")
+                    val lastKnown = locationManager.getLastKnownLocation(provider)
+                    Log.e("LocationService", "Last known: $lastKnown")
+                    continuation.resume(lastKnown) { cause, _, _ -> (::onCancellation)(cause) }
+                }
+            }
+        }
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private suspend fun getLocationViaGms(): Location? {
+        return suspendCancellableCoroutine { continuation ->
+            fusedLocationClient?.getCurrentLocation( // cannot be null if GMS is available
+                Priority.PRIORITY_HIGH_ACCURACY,
+                null // cancellation signal
+            )?.addOnSuccessListener { location ->
+                continuation.resume(location) { cause, _, _ -> onCancellation(cause) }
+            }?.addOnFailureListener {
+                continuation.resume(null) { cause, _, _ -> onCancellation(cause) }
+            }
+        }
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    suspend fun getCurrentLocation(): Location? {
+        return formatLocation(if (isGmsAvailable()) {
+            getLocationViaGms()
+        } else {
+            getLocationViaAndroid()
+        })
+    }
+
     private fun isGmsAvailable(): Boolean {
         return GoogleApiAvailability.getInstance()
             .isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS
     }
 
-    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
-    suspend fun getCurrentLocation(): Location? {
-        if (isGmsAvailable()) {
 
-            return suspendCancellableCoroutine { continuation ->
-                fusedLocationClient?.getCurrentLocation( // cannot be null if GMS is available
-                    Priority.PRIORITY_HIGH_ACCURACY,
-                    null // cancellation signal
-                )?.addOnSuccessListener { location ->
-                    continuation.resume(location) { cause, _, _ -> onCancellation(cause) }
-                }?.addOnFailureListener {
-                    continuation.resume(null) { cause, _, _ -> onCancellation(cause) }
-                }
-            }
+    private fun formatLocation(location : Location?): Location? {
+        return if (location == null) {
+            null
         } else {
-            return suspendCancellableCoroutine { continuation ->
-                val provider = when {
-                    locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) -> {
-                        Log.e("LocationService", "Using GPS provider")
-                        LocationManager.GPS_PROVIDER
-                    }
-                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) -> {
-                        Log.e("LocationService", "Using Network provider")
-                        LocationManager.NETWORK_PROVIDER
-                    }
-                    else -> {
-                        Log.e("LocationService", "No provider available")
-                        null
-                    }
-                }
-
-                if (provider == null) {
-                    continuation.resume(null) { cause, _, _ -> (::onCancellation)(cause) }
-                    return@suspendCancellableCoroutine
-                }
-
-                Log.e("LocationService", "Requesting location from $provider")
-                locationManager.getCurrentLocation(
-                    provider,
-                    null,
-                    context.mainExecutor
-                ) { location ->
-                    if (location != null) {
-                        Log.e("LocationService", "Got fresh location: $location")
-                        continuation.resume(location) { cause, _, _ -> (::onCancellation)(cause) }
-                    } else {
-                        // cold fix, try last known
-                        Log.e("LocationService", "Fresh location null, trying last known")
-                        val lastKnown = locationManager.getLastKnownLocation(provider)
-                        Log.e("LocationService", "Last known: $lastKnown")
-                        continuation.resume(lastKnown) { cause, _, _ -> (::onCancellation)(cause) }
-                    }
-                }
-            }
+            val decimals = 5 // https://xkcd.com/2170/
+            location.latitude  = location.latitude.toBigDecimal().setScale(decimals, RoundingMode.HALF_EVEN).toDouble()
+            location.longitude = location.longitude.toBigDecimal().setScale(decimals, RoundingMode.HALF_EVEN).toDouble()
+            location
         }
     }
 
--- app/src/main/java/com/ggs/parkuzpp/ui/MapScreen.kt
+++ app/src/main/java/com/ggs/parkuzpp/ui/MapScreen.kt
@@ -4,10 +4,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import com.ggs.parkuzpp.R
+import com.ggs.parkuzpp.location.UserTriggeredGPSService
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun MapScreen(
@@ -15,20 +25,49 @@ fun MapScreen(
     onNavigateToAccount: () -> Unit,
     onNavigateToCamera: () -> Unit
 ) {
+    val noLocationText = stringResource(R.string.no_location_yet)
+    val couldNotGetLocationText = stringResource(R.string.could_not_get_location)
+    val saveSpotText = stringResource(R.string.save_parking_spot)
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val gps = remember { UserTriggeredGPSService(context) }
+    var coordinates by remember { mutableStateOf(noLocationText) }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         // --- Miejsce na Mapę ---
-         Box(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "MAPA (placeholder)",
-                fontSize = 20.sp
-            )
+            Column {
+
+                Text(
+                    text = "MAPA (placeholder)",
+                    fontSize = 20.sp
+                )
+                Text(text = coordinates)
+                Button(onClick = {
+                    scope.launch {
+                        try {
+                            val location = gps.getCurrentLocation()
+                            coordinates = if (location != null) {
+                                "Lat: ${location.latitude}, Lng: ${location.longitude}"
+                            } else {
+                                couldNotGetLocationText
+                            }
+                        } catch (_: SecurityException) {
+                            coordinates = couldNotGetLocationText
+                        }
+                    }
+                }) {
+                    Text(saveSpotText)
+                }
+            }
         }
 
         // --- Dolne Menu ---
